b14abd39d7cbb4c1efe938a4502f86c47ef45d91
Tatiana <tarzhakova@gmail.com>
2022-02-14 11:41:35 +0100

    formatNumber - accept in wei, delete leading zero

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,17 +1,28 @@
-/** 1234567.890100 => 1,234,567.8901 */
-const formatNumber = (x, reduceDecimalTo) => {
-  if (!x) return
+import Web3 from 'web3'
+import bnUtils from './bnUtils'
+
+/**
+ * "001234500600700800900100200" => "1,234,500.6007008009001002"
+ * "0x10" => "0.000000000000000016"
+ * @param {string} number inWei, !not a BigNumber object
+ * @param {integer} [reduceDecimalTo]
+ */
+const formatNumber = (number, reduceDecimalTo = 18) => {
+  if (!number) return
+  const web3 = new Web3()
+  const x = bnUtils.toFixed(web3.utils.fromWei(number), reduceDecimalTo);
   (x + '').replace(',', '.')
   const a = x.split('.')
+  const integer = a[0].toString().replace(/\b0+(?!$)/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   if (a[1]) {
     if (reduceDecimalTo) {
       const decimals = a[1].substring(0, reduceDecimalTo).replace(/0+$/, '')
-      return a[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + `${decimals ? '.' + decimals : ''}`
+      return integer + `${decimals ? '.' + decimals : ''}`
     } else {
-      return a[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + '.' + a[1]
+      return integer + '.' + a[1]
     }
   } else {
-    return a[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    return integer
   }
 }
 
