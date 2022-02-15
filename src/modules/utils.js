d5a6db6f6bc488f7f32c667a0d3cbbcfaaa9087d
Tatiana <tarzhakova@gmail.com>
2022-02-15 09:47:56 +0100

    delete import bnUtils to formatNumber

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,5 +1,4 @@
 import Web3 from 'web3'
-import bnUtils from './bnUtils'
 
 /**
  * "001234500600700800900100200" => "1,234,500.6007008009001002"
@@ -10,8 +9,8 @@ import bnUtils from './bnUtils'
 const formatNumber = (number, reduceDecimalTo = 18) => {
   if (!number) return
   const web3 = new Web3()
-  const x = bnUtils.toFixed(web3.utils.fromWei(number), reduceDecimalTo);
-  (x + '').replace(',', '.')
+  const x = web3.utils.fromWei(number)
+  // (x + '').replace(',', '.')
   const a = x.split('.')
   const integer = a[0].toString().replace(/\b0+(?!$)/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   if (a[1]) {
